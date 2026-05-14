f24ba17e42c1aec18759b0734840fe2fa326b43a
Adérito Marrime <153687419+aderitomarrime@users.noreply.github.com>
2026-05-14 19:33:12 +0200

    Add real time year update to footer

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,11 @@
 
 let body = document.querySelector("body");
 let button = document.querySelector("nav button");
+let thisYear = document.querySelector(".year");
+
+let realDate = new Date();
+
+thisYear.textContent = `${realDate.getFullYear()}`;
 
 button.addEventListener("click", toggleMode);
 
