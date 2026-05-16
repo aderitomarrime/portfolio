2baf72bdeefd36898c328b5809440979df00d923
Adérito Marrime <153687419+aderitomarrime@users.noreply.github.com>
2026-05-16 14:55:40 +0200

    Add responsiveness for screens with width less or equal 640px

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,6 +5,7 @@ let buttonMobile = document.querySelector(".nav-mobile button:last-child");
 let buttonMenu = document.querySelector(".menu");
 let buttonClose = document.querySelector(".close");
 let navMobile = document.querySelector(".nav-mobile")
+let navLinksMobile = document.querySelectorAll(".bottom-elements ul")
 let thisYear = document.querySelector(".year");
 
 let realDate = new Date();
@@ -14,7 +15,9 @@ thisYear.textContent = `${realDate.getFullYear()}`;
 buttonMobile.addEventListener("click", toggleMode);
 buttonDesktop.addEventListener("click", toggleMode);
 buttonMenu.addEventListener("click", showNav);
-buttonClose.addEventListener("click", closeNav)
+buttonClose.addEventListener("click", closeNavByCloseButton)
+
+navLinksMobile.forEach(closeNavByLinks);
 
 function toggleMode() {
     body.classList.toggle("dark");
@@ -24,6 +27,12 @@ function showNav() {
     navMobile.setAttribute("style", "left: 0")
 }
 
-function closeNav() {
+function closeNavByCloseButton() {
     navMobile.setAttribute("style", "left: -100%")
+}
+
+function closeNavByLinks(item) {
+    item.addEventListener("click", ()=> {
+        navMobile.setAttribute("style", "left: -100%")
+    })
 }
